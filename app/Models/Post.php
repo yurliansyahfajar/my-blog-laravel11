85f43f259c1880787922cffa0bd046c941b1f906
yurliansyahf <yurliansyahf@gmail.com>
2024-12-25 10:10:30 +0700

    add post factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'author',
